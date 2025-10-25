47f350760dc9bf5c5c8f34cd5240a7f911fd2e69
MaxiPTV Update System <maiptv1987@gmail.com>
2025-10-24 20:31:52 -0400

    Release v1.0.89 - Build 89

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.maxiptv"
     minSdk = 21
     targetSdk = 34
-        versionCode = 87
-        versionName = "1.0.87"
+        versionCode = 89
+        versionName = "1.0.89"
     vectorDrawables.useSupportLibrary = true
     buildConfigField("String", "DEFAULT_PLAYER_API", "\"https://aztv.cx/\"")
     buildConfigField("String", "DEFAULT_USER", "\"max\"")
