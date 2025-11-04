220185a99b16bc22ab0caaccaf2a4da579ceb693
MaxiPTV Update System <maiptv1987@gmail.com>
2025-11-04 17:45:54 -0500

    Release v1.0.133 - Build 133

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.maxiptv"
     minSdk = 21
     targetSdk = 34
-        versionCode = 132
-        versionName = "1.0.132"
+        versionCode = 133
+        versionName = "1.0.133"
     vectorDrawables.useSupportLibrary = true
     buildConfigField("String", "DEFAULT_PLAYER_API", "\"https://aztv.cx/\"")
     buildConfigField("String", "DEFAULT_USER", "\"max\"")
