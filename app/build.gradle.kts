ff65ab5cc4aa278848d64c0f4bfe063e051d7d46
MaxiPTV Update System <maiptv1987@gmail.com>
2025-11-14 22:49:05 -0500

    Release v1.0.194 - Build 194

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.maxiptv"
     minSdk = 21
     targetSdk = 34
-        versionCode = 196
-        versionName = "1.0.196"
+        versionCode = 194
+        versionName = "1.0.194"
     vectorDrawables.useSupportLibrary = true
     ndk {
       abiFilters += listOf("armeabi-v7a", "arm64-v8a")
--- app/build/generated/source/buildConfig/release/com/maxiptv/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/com/maxiptv/BuildConfig.java
@@ -7,8 +7,8 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.maxiptv";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 196;
-  public static final String VERSION_NAME = "1.0.196";
+  public static final int VERSION_CODE = 194;
+  public static final String VERSION_NAME = "1.0.194";
   // Field from default config.
   public static final String DEFAULT_PASS = "1h2yd90";
   // Field from default config.
