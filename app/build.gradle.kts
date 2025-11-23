5ff1e31c8858177bd12af31286def47787e8c19f
MaxiPTV Update System <maiptv1987@gmail.com>
2025-11-22 22:40:13 -0500

    Release v1.0.292 - Build 292

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.maxiptv"
     minSdk = 21
     targetSdk = 34
-        versionCode = 291
-        versionName = "1.0.291"
+        versionCode = 292
+        versionName = "1.0.292"
     vectorDrawables.useSupportLibrary = true
     ndk {
       abiFilters += listOf("armeabi-v7a", "arm64-v8a")
